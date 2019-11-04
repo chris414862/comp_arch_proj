6e23656034f509392a50026e8cc789dbf2089742
Zac <36095352+DefinableEarth4@users.noreply.github.com>
2019-11-04 09:15:07 -0700

    Add files via upload
    Added Parsing, Only need to finish Calculation

--- Sim.py
+++ Sim.py
@@ -1,6 +1,8 @@
 import sys
 import os
+from os import path
 import argparse
+
 
 # -f <trace file name>     [ name of text file with the trace ]
 # -s <cache block size>    [ 1 KB to 8 MB ]
@@ -42,4 +44,80 @@ associativity = args.associativity
 rep_policy = args.rep_policy
 
 
-        
+#File validation and parse
+if not path.exists(trace_file):
+        print("File '", trace_file, "' doen't exist")
+        exit()
+
+with open(trace_file) as f:
+        count = 0 # to complete one block of EIP and dstM
+        length = 0;         #bytes read
+        br = 0;             #bytes to read
+        hex_value1 = ''
+        hex_write = ''
+        hex_read = ''
+        return_statement = " "
+        for line in f:
+                datarw = 0 # 1 for read, 2 for write, 3 for read/write
+                length = 0;         #bytes read
+                if line[0] == 'E':
+                        line = line.rstrip('\n')
+                        #print(line)
+                        br = int(line[5 : 7], 16)
+                        hex_value1 = line[10 : 18]
+                        #start line 19 for codes
+                        for x in line:
+                                if x == ' ' and line[length + 1] == ' ':
+                                        break
+                                else:
+                                        length = length + 1
+                        #print(line[19 : length])
+                        #
+                        #Need to finish the length because it changes
+                        #
+                        count += 1
+                        
+
+                if line[0] == 'd':
+                        
+                        line = line.rstrip('\n')
+                        #print(line)
+
+                        hex_write = line[6 : 14]
+                        if hex_write != '00000000':
+                                if datarw == 2:
+                                        datarw = 3
+                                if datarw == 0:
+                                        datarw = 1
+
+                        hex_read = line[33 : 41]
+                        if hex_read != '00000000':
+                                if datarw == 1:
+                                        datarw = 3
+                                if datarw == 0:
+                                        datarw = 2
+                        
+                        count += 1
+                if count == 2:
+                        return_statement = "Address: 0x" + hex_value1 + ", length = " + str(br)
+                        if datarw == 0:
+                                return_statement = return_statement + ". No data writes/reads occurred."
+                        if datarw == 2:
+                                return_statement = return_statement + ". Data read at 0x" + hex_read + ", length = 4 bytes."
+                        if datarw == 3:
+                                return_statement = return_statement + ". Data write at 0x" + hex_write + ", length = 4 bytes."
+                        if datarw == 3:
+                                return_statement = return_statement + ". Data write at 0x" + hex_write + ", length = 4 bytes, data read at 0x" + hex_read + ", length = 4 bytes."
+                        count = 0
+                        print(return_statement + '\n')
+                        return_statement = " "
+                        #get rid of to load rest of the file        
+                        
+                        
+
+
+
+
+f.close()
+
+
