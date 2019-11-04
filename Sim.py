191ab9daf862cfe50133d884642ba98e5bb8a3eb
Chris Crabtree <chris414862@gmail.com>
2019-11-04 13:40:28 -0600

    Added OO implementation option

--- Sim.py
+++ Sim.py
@@ -10,31 +10,36 @@ import argparse
 # -a <associativity>       [ 1, 2, 4, 8, 16 ]
 # -r <replacement policy>  [RR or RND or LRU for bonus points]
 
+def parse_args():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-f', dest='trace_file', metavar='filename', type=str
+        				   , required=True, help='filename of the trace file')
+        parser.add_argument('-s', dest='cache_size', metavar='cache size',type=int
+        				   , required=True
+        				   , help='Size of cache in kb. Range is 1 to 8192')
+        parser.add_argument('-b', dest='block_size', metavar='block size',type=int
+        				   , required=True
+        				   , help='Size of blocks in kb. Range is 4 to 64')
+        parser.add_argument('-a', dest='associativity', metavar='associativity'
+        				   ,type=int, choices=[1,2,4,8,16], required=True
+        				   , help='Sets the associativity. Acceptable'\
+        				   +' values are: 1, 2, 4, 8, 16')
+        parser.add_argument('-r', dest='rep_policy', metavar='replacement',type=str
+        				   , choices=['RR','RND','LRU'], required=True
+        				   , help='Replacement policy. Acceptable values: RR (round'\
+        				   +' robin), RND (random), LRU (least recently used)')
+        parser.add_argument('-d', dest='debug', action='store_true', help='Add extra print statements')
+        args = parser.parse_args()
+        if not (1 <= args.cache_size <= 8192):
+        	parser.error('-s cache size is out of range.  int between 1 and 8192') 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', dest='trace_file', metavar='filename', type=str
-				   , required=True, help='filename of the trace file')
-parser.add_argument('-s', dest='cache_size', metavar='cache size',type=int
-				   , required=True
-				   , help='Size of cache in kb. Range is 1 to 8192')
-parser.add_argument('-b', dest='block_size', metavar='block size',type=int
-				   , required=True
-				   , help='Size of blocks in kb. Range is 4 to 64')
-parser.add_argument('-a', dest='associativity', metavar='associativity'
-				   ,type=int, choices=[1,2,4,8,16], required=True
-				   , help='Sets the associativity. Acceptable'\
-				   +' values are: 1, 2, 4, 8, 16')
-parser.add_argument('-r', dest='rep_policy', metavar='replacement',type=str
-				   , choices=['RR','RND','LRU'], required=True
-				   , help='Replacement policy. Acceptable values: RR (round'\
-				   +' robin), RND (random), LRU (least recently used)')
-args = parser.parse_args()
-if not (1 <= args.cache_size <= 8192):
-	parser.error('-s cache size is out of range.  int between 1 and 8192') 
+        if not (4 <= args.block_size <= 64):
+        	parser.error('-b block size is out of range. Select int between 4 and 64') 
 
-if not (1 <= args.block_size <= 64):
-	parser.error('-b block size is out of range. Select int between 1 and 64') 
+        return args
 
+#### Script style implementation
+args = parse_args()       
 print(args)
 
 trace_file = args.trace_file
@@ -42,9 +47,10 @@ cache_size = args.cache_size
 block_size = args.block_size
 associativity = args.associativity
 rep_policy = args.rep_policy
+group_number = 14
 
 
-#File validation and parse
+# File validation and parse
 if not path.exists(trace_file):
         print("File '", trace_file, "' doen't exist")
         exit()
@@ -67,7 +73,9 @@ with open(trace_file) as f:
                         hex_value1 = line[10 : 18]
                         #start line 19 for codes
                         for x in line:
-                                if x == ' ' and line[length + 1] == ' ':
+                                # *** CC: I added a check to make sure the string isn't
+                                # accessed out of range
+                                if x == ' ' and len(line) > length + 1 and line[length + 1] == ' ':
                                         break
                                 else:
                                         length = length + 1
@@ -117,7 +125,186 @@ with open(trace_file) as f:
 
 
 
-
 f.close()
+
+#### Object Oriented Implementation \w some modularization ####
+# class mem_access_request():
+#         def __init__(self, address=None, access_type=None, length=None):
+#                 # Error handling for address. (To catch programmer errors)
+#                 if type(address) is not None and type(address) != str and type(address) != int:
+#                         raise ValueError("address must either be a string or a base 16 int")
+#                 if type(address) == str:
+#                         try:
+#                                 address = int(address, 16)
+#                         except ValueError as e:
+#                                 print('address was not a number')
+#                                 raise e
+
+#                 self.address = address
+
+#                 # Error handling for access_type
+#                 if (type(access_type) == str and access_type not in ['i','w','r']) \
+#                     and type(access_type) is not None:
+#                         raise ValueError("access_type must be 'i' (instruction), 'w' (write),"
+#                                         +" 'r' (read). "+str(access_type)+" is an invalid value.")
+
+#                 self.access_type = access_type
+
+#                 if self.access_type == 'w' or self.access_type == 'r':
+#                         self.length = 4
+#                 else:
+#                         try:
+#                                 self.length = int(length)
+#                         except ValueError as e:
+#                                 print('length was not a number')
+#                                 raise e
+
+#         def to_string(self):
+#                 s = '0x%x'%(self.address)+' ('+str(self.length)+')'
+#                 return s
+
+
+# class full_instruction():
+#         def __init__(self,instruction=None, read=None, write=None, instruct_line=None
+#                     , rw_line=None):
+#                 self.instruction = instruction
+#                 self.read = read
+#                 self.write = write
+#                 self.instruct_line = instruct_line
+#                 self.rw_line=rw_line
+
+#         def display(self, debug=False, num_print=3):
+#                 instruction_count = 1
+#                 if debug:
+#                         print('***instruction request:')
+
+#                 print(self.instruction.to_string())
+
+#                 if debug:
+#                         print('from line:', self.instruct_line)
+#                 if instruction_count == num_print:
+#                                 return instruction_count
+
+#                 if debug:
+#                         print('write request:')
+#                 if self.write is not None:
+#                         instruction_count += 1
+#                         print(self.write.to_string())
+
+#                 if debug:
+#                         if self.write is None:
+#                                 print(self.write)
+#                         print('from line:', self.rw_line)
+#                 if instruction_count == num_print:
+#                                 return instruction_count
+
+#                 if debug:
+#                         print('read request:')
+#                 if self.read is not None:
+#                         instruction_count += 1
+#                         print(self.read.to_string())
+#                 if debug:
+#                         if self.read is None:
+#                                 print(self.read)
+#                         print('from line:', self.rw_line)       
+                
+#                 return instruction_count
+
+
+
+# def print_header(cmd_args, parsed_args):
+#         print('\nCmd Line:', ' '.join(cmd_args))
+#         print('Trace File:', parsed_args.trace_file)
+#         print('Cache Size:', parsed_args.cache_size)
+#         print('Block Size:', parsed_args.block_size)
+#         print('Associativity:', parsed_args.associativity)
+#         print('R-Policy:', parsed_args.rep_policy)
+
+# def print_cache_info():
+#         #TODO: fill out actual values after implementing calculations
+#         print('----- Calculated Values -----')
+#         print('Total #Blocks:')
+#         print('Tag Size:')
+#         print('Index Size:')
+#         print('Overhead Memory Size:')
+#         print('Implementation Memory Size:')
+
+# def print_results():
+#         #TODO: fill out results once cache is implemented
+#         print('----- Results -----')
+#         print('Cache Hit Rate:')
+#         print('Cache Miss Rate:')
+#         print('CPI:', '\n')
+
+
+# def print_samples(instructions, num_print=20, debug=False):
+#         count = 0
+#         for instruct in instructions:
+#                 count+= instruct.display(num_print=(num_print-count), debug=debug)
+#                 if count >= num_print:
+#                         if debug:
+#                                 print('num_print reached:', num_print)
+#                         break
+
+# def read_instructions(trace_file):
+#         if not path.exists(trace_file):
+#                 print("File '", trace_file, "' doen't exist")
+#                 exit()
+#         with open(trace_file) as f:
+#                 instruction_list = []
+#                 next_full_instruct = None
+#                 for line in f:
+#                         if line[0] == 'E':
+#                                 length = line[5:7]
+#                                 address = line[10 : 18]
+#                                 access_type = 'i'
+#                                 instruction = mem_access_request(address=address
+#                                                                 , access_type=access_type
+#                                                                 , length=length)
+#                                 next_full_instruct = full_instruction(instruction=instruction)
+#                                 next_full_instruct.instruct_line = line
+#                         if line[0] == 'd':
+#                                 if next_full_instruct == None:
+#                                         print('Logic error: processed read/write before '
+#                                              +'instruction line')
+#                                         raise
+
+#                                 write_address = line[6:14]
+#                                 write = None
+#                                 if write_address != '00000000':
+#                                         write = mem_access_request(address=write_address
+#                                                                   , access_type='w')
+                                
+#                                 read_address = line[33:41]
+#                                 read = None
+#                                 if read_address != '00000000':
+#                                         read = mem_access_request(address=read_address
+#                                                                  , access_type='r')
+
+#                                 next_full_instruct.write = write
+#                                 next_full_instruct.read = read
+#                                 next_full_instruct.rw_line = line
+#                                 if next_full_instruct.instruction.length > 0:
+#                                         instruction_list.append(next_full_instruct)
+
+#                                 # reset for next set of instructions
+#                                 next_full_instruct = None
+
+#         return instruction_list
+
+
+# def main():
+#         args = parse_args()
+#         print_header(sys.argv, args)
+
+#         print_cache_info()
+#         full_instructions = read_instructions(args.trace_file)
+
+#         print_results()
+#         print_samples(full_instructions, num_print=20, debug=args.debug)    
+
+
+# if __name__ == '__main__':
+#         main()
 
 
