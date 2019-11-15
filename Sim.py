1d297f1a6a7fb69a06258d5958d557f2022b04c0
Zac <36095352+DefinableEarth4@users.noreply.github.com>
2019-11-15 08:17:07 -0700

    Added Cache implementation
    I could not find how to calculate Overhead Memory

--- Sim.py
+++ Sim.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 import math
@@ -49,14 +50,16 @@ block_size = args.block_size
 associativity = args.associativity
 rep_policy = args.rep_policy
 group_number = 14
-
+total_blocks = cache_size / block_size
+number_of_indexes = (cache_size * 1000 ) / (block_size * associativity) # of indexes with associativity
+block_offset_bits = int(math.log(block_size, 2)) # get the offset bits
+index_bits = int(math.log(number_of_indexes, 2))+1  # get the index bits
+tag_bits = 32 - block_offset_bits - index_bits  #tag bits 
 
 # File validation
 if not path.exists(trace_file):
         print("File '", trace_file, "' doen't exist")
         exit()
-
-
 
 
 #
@@ -65,74 +68,14 @@ if not path.exists(trace_file):
 # python3 Sim.py -f TestTrace.trc -s 1024 -b 16 -a 2 -r RR
 #
 # Based on this implementation of cache, there will be a dictionary of dictionaries
-# 
-# Of associativity n:
-# 
-# cache = { 0 : cache_set, 1 : cache_set, ... , n-1 : cache_set}
-# 
-# For example, cache size is 1024KB, 
-# 2-way set associative. Block size: 16
-# There would be 2^15 indexes (rows) per cache_set * 2 (associativity)
-# 
-# cache = { 0 : cache_set, 1 : cache_set }
-#
-#               cache:
-# 
-# 0 : cache_set: , 1 :  cache_set:
-#      [  0   ]          [  0   ]
-#      [  1   ]          [  1   ]
-#      [  .   ]          [  .   ]
-#      [  .   ]          [  .   ]
-#      [  .   ]          [  .   ]
-#      [ 2^15 ]          [ 2^15 ]
 
-cache = {} #empty cache set
-cache_set = {} #empty cache for indexes
-x = 0      # counter
-
-number_of_indexes = (cache_size * 1000 ) / (block_size * associativity) # of indexes with associativity
-
-block_offset_bits = int(math.log(block_size, 2)) # get the offset bits
-index_bits = int(math.log(number_of_indexes, 2))+1  # get the index bits
-tag_bits = 32 - block_offset_bits - index_bits 
-
-# I believe these calculations are correct 
-print("Temporary Calculations")
-print("Number of indexes with associativity " + str(int(number_of_indexes)) + " 2^" + str(index_bits))
-print(str(block_offset_bits + index_bits + tag_bits) + " bits total" )
-print(str(block_offset_bits) + " bit offset")
-print(str(index_bits) + " bit index")
-print(str(tag_bits) + " bit tag")
-
+#empty cache set
+cache = {}
 
 #This creates the cache using index size with associativity
-while x < number_of_indexes:
-        num = hex(x) 
-        cache_set[num] =  "00000000" # Example entry will be { 0x7cff : "00000000" }
-        x += 1
+cache = {i : {a : (0, None) for a in range(int(associativity))} for i in range(int(number_of_indexes))}
 
-# Uncomment print(cache_set) to see all of the 
-# indexes in 1/n section of the cache
-# This does print unordered
-# print(cache_set)
-
-
-#this the creates x (associativity) number for all the memory
-x = 0
-while x < associativity:
-        cache.update({x : cache_set}) # adds entry into dictionary
-        x+=1
-
-# uncomment print(cache) to see entire empty cache
-# print (cache) 
-
-
-
-
-
-
-
-
+#print (cache) 
 
 
 #parsing file
@@ -198,181 +141,186 @@ with open(trace_file) as f:
 f.close()
 
 #### Object Oriented Implementation \w some modularization ####
-# class mem_access_request():
-#         def __init__(self, address=None, access_type=None, length=None):
-#                 # Error handling for address. (To catch programmer errors)
-#                 if type(address) is not None and type(address) != str and type(address) != int:
-#                         raise ValueError("address must either be a string or a base 16 int")
-#                 if type(address) == str:
-#                         try:
-#                                 address = int(address, 16)
-#                         except ValueError as e:
-#                                 print('address was not a number')
-#                                 raise e
+class mem_access_request():
+    def __init__(self, address=None, access_type=None, length=None):
+        # Error handling for address. (To catch programmer errors)
+        if type(address) is not None and type(address) != str and type(address) != int:
+            raise ValueError("address must either be a string or a base 16 int")
+        if type(address) == str:
+            try:
+                address = int(address, 16)
+            except ValueError as e:
+                print('address was not a number')
+                raise e
 
-#                 self.address = address
+        self.address = address
 
-#                 # Error handling for access_type
-#                 if (type(access_type) == str and access_type not in ['i','w','r']) \
-#                     and type(access_type) is not None:
-#                         raise ValueError("access_type must be 'i' (instruction), 'w' (write),"
-#                                         +" 'r' (read). "+str(access_type)+" is an invalid value.")
+        # Error handling for access_type
+        if (type(access_type) == str and access_type not in ['i','w','r']) \
+            and type(access_type) is not None:
+                raise ValueError("access_type must be 'i' (instruction), 'w' (write),"
+                                 +" 'r' (read). "+str(access_type)+" is an invalid value.")
 
-#                 self.access_type = access_type
+        self.access_type = access_type
 
-#                 if self.access_type == 'w' or self.access_type == 'r':
-#                         self.length = 4
-#                 else:
-#                         try:
-#                                 self.length = int(length)
-#                         except ValueError as e:
-#                                 print('length was not a number')
-#                                 raise e
+        if self.access_type == 'w' or self.access_type == 'r':
+            self.length = 4
+        else:
+            try:
+                self.length = int(length)
+            except ValueError as e:
+                print('length was not a number')
+                raise e
 
-#         def to_string(self):
-#                 s = '0x%x'%(self.address)+' ('+str(self.length)+')'
-#                 return s
+    def to_string(self):
+        s = '0x%x'%(self.address)+' ('+str(self.length)+')'
+        return s
 
 
-# class full_instruction():
-#         def __init__(self,instruction=None, read=None, write=None, instruct_line=None
-#                     , rw_line=None):
-#                 self.instruction = instruction
-#                 self.read = read
-#                 self.write = write
-#                 self.instruct_line = instruct_line
-#                 self.rw_line=rw_line
+class full_instruction():
+    def __init__(self,instruction=None, read=None, write=None, instruct_line=None
+                    , rw_line=None):
+        self.instruction = instruction
+        self.read = read
+        self.write = write
+        self.instruct_line = instruct_line
+        self.rw_line=rw_line
 
-#         def display(self, debug=False, num_print=3):
-#                 instruction_count = 1
-#                 if debug:
-#                         print('***instruction request:')
+    def display(self, debug=False, num_print=3):
+        instruction_count = 1
+        if debug:
+            print('***instruction request:')
 
-#                 print(self.instruction.to_string())
+        print(self.instruction.to_string())
 
-#                 if debug:
-#                         print('from line:', self.instruct_line)
-#                 if instruction_count == num_print:
-#                                 return instruction_count
+        if debug:
+            print('from line:', self.instruct_line)
+        if instruction_count == num_print:
+            return instruction_count
 
-#                 if debug:
-#                         print('write request:')
-#                 if self.write is not None:
-#                         instruction_count += 1
-#                         print(self.write.to_string())
+        if debug:
+            print('write request:')
+        if self.write is not None:
+            instruction_count += 1
+            print(self.write.to_string())
 
-#                 if debug:
-#                         if self.write is None:
-#                                 print(self.write)
-#                         print('from line:', self.rw_line)
-#                 if instruction_count == num_print:
-#                                 return instruction_count
+        if debug:
+            if self.write is None:
+                print(self.write)
+            print('from line:', self.rw_line)
+        if instruction_count == num_print:
+            return instruction_count
 
-#                 if debug:
-#                         print('read request:')
-#                 if self.read is not None:
-#                         instruction_count += 1
-#                         print(self.read.to_string())
-#                 if debug:
-#                         if self.read is None:
-#                                 print(self.read)
-#                         print('from line:', self.rw_line)       
+        if debug:
+            print('read request:')
+        if self.read is not None:
+            instruction_count += 1
+            print(self.read.to_string())
+        if debug:
+            if self.read is None:
+                print(self.read)
+            print('from line:', self.rw_line)       
                 
-#                 return instruction_count
+        return instruction_count
+
+class cache():
+    def __init__ (self, total_blocks=None, number_of_indexes=None, block_offset=None, index_bits=None, tag_bits=None):
+        self.total_blocks = parsed_args.cache_size / parsed_args.cache_size
+        self.number_of_indexes = (cache_size * 1000 ) / (block_size * associativity) # of indexes with associativity
+        self.block_offset_bits = int(math.log(block_size, 2)) # get the offset bits
+        self.index_bits = int(math.log(number_of_indexes, 2))+1  # get the index bits
+        self.tag_bits = 32 - block_offset_bits - index_bits  #tag bits
+    
+def print_header(cmd_args, parsed_args):
+        print('\nCmd Line:', ' '.join(cmd_args))
+        print('Trace File:', parsed_args.trace_file)
+        print('Cache Size:', parsed_args.cache_size)
+        print('Block Size:', parsed_args.block_size)
+        print('Associativity:', parsed_args.associativity)
+        print('R-Policy:', parsed_args.rep_policy)
+
+def print_cache_info():
+        #TODO: fill out actual values after implementing calculations
+        print('----- Calculated Values -----')
+        print('Total #Blocks:', int(total_blocks)  )
+        print('Tag Size:', tag_bits)
+        print('Index Size:', int(number_of_indexes))
+        print('Overhead Memory Size:')
+        print('Implementation Memory Size:')
+
+def print_results():
+        #TODO: fill out results once cache is implemented
+        print('----- Results -----')
+        print('Cache Hit Rate:')
+        print('Cache Miss Rate:')
+        print('CPI:', '\n')
 
 
+def print_samples(instructions, num_print=20, debug=False):
+        count = 0
+        for instruct in instructions:
+                count+= instruct.display(num_print=(num_print-count), debug=debug)
+                if count >= num_print:
+                        if debug:
+                                print('num_print reached:', num_print)
+                        break
 
-# def print_header(cmd_args, parsed_args):
-#         print('\nCmd Line:', ' '.join(cmd_args))
-#         print('Trace File:', parsed_args.trace_file)
-#         print('Cache Size:', parsed_args.cache_size)
-#         print('Block Size:', parsed_args.block_size)
-#         print('Associativity:', parsed_args.associativity)
-#         print('R-Policy:', parsed_args.rep_policy)
+def read_instructions(trace_file):
+        if not path.exists(trace_file):
+                print("File '", trace_file, "' doen't exist")
+                exit()
+        with open(trace_file) as f:
+                instruction_list = []
+                next_full_instruct = None
+                for line in f:
+                        if line[0] == 'E':
+                                length = line[5:7]
+                                address = line[10 : 18]
+                                access_type = 'i'
+                                instruction = mem_access_request(address=address
+                                                                , access_type=access_type
+                                                                , length=length)
+                                next_full_instruct = full_instruction(instruction=instruction)
+                                next_full_instruct.instruct_line = line
+                        if line[0] == 'd':
+                                if next_full_instruct == None:
+                                        print('Logic error: processed read/write before '
+                                            +'instruction line')
+                                        raise
 
-# def print_cache_info():
-#         #TODO: fill out actual values after implementing calculations
-#         print('----- Calculated Values -----')
-#         print('Total #Blocks:')
-#         print('Tag Size:')
-#         print('Index Size:')
-#         print('Overhead Memory Size:')
-#         print('Implementation Memory Size:')
-
-# def print_results():
-#         #TODO: fill out results once cache is implemented
-#         print('----- Results -----')
-#         print('Cache Hit Rate:')
-#         print('Cache Miss Rate:')
-#         print('CPI:', '\n')
-
-
-# def print_samples(instructions, num_print=20, debug=False):
-#         count = 0
-#         for instruct in instructions:
-#                 count+= instruct.display(num_print=(num_print-count), debug=debug)
-#                 if count >= num_print:
-#                         if debug:
-#                                 print('num_print reached:', num_print)
-#                         break
-
-# def read_instructions(trace_file):
-#         if not path.exists(trace_file):
-#                 print("File '", trace_file, "' doen't exist")
-#                 exit()
-#         with open(trace_file) as f:
-#                 instruction_list = []
-#                 next_full_instruct = None
-#                 for line in f:
-#                         if line[0] == 'E':
-#                                 length = line[5:7]
-#                                 address = line[10 : 18]
-#                                 access_type = 'i'
-#                                 instruction = mem_access_request(address=address
-#                                                                 , access_type=access_type
-#                                                                 , length=length)
-#                                 next_full_instruct = full_instruction(instruction=instruction)
-#                                 next_full_instruct.instruct_line = line
-#                         if line[0] == 'd':
-#                                 if next_full_instruct == None:
-#                                         print('Logic error: processed read/write before '
-#                                              +'instruction line')
-#                                         raise
-
-#                                 write_address = line[6:14]
-#                                 write = None
-#                                 if write_address != '00000000':
-#                                         write = mem_access_request(address=write_address
-#                                                                   , access_type='w')
+                                write_address = line[6:14]
+                                write = None
+                                if write_address != '00000000':
+                                        write = mem_access_request(address=write_address
+                                                                    , access_type='w')
                                 
-#                                 read_address = line[33:41]
-#                                 read = None
-#                                 if read_address != '00000000':
-#                                         read = mem_access_request(address=read_address
-#                                                                  , access_type='r')
+                                read_address = line[33:41]
+                                read = None
+                                if read_address != '00000000':
+                                        read = mem_access_request(address=read_address
+                                                                , access_type='r')
 
-#                                 next_full_instruct.write = write
-#                                 next_full_instruct.read = read
-#                                 next_full_instruct.rw_line = line
-#                                 if next_full_instruct.instruction.length > 0:
-#                                         instruction_list.append(next_full_instruct)
+                                next_full_instruct.write = write
+                                next_full_instruct.read = read
+                                next_full_instruct.rw_line = line
+                                if next_full_instruct.instruction.length > 0:
+                                        instruction_list.append(next_full_instruct)
 
-#                                 # reset for next set of instructions
-#                                 next_full_instruct = None
-
-#         return instruction_list
-
-
-# def main():
-#         args = parse_args()
-#         print_header(sys.argv, args)
-
-#         print_cache_info()
-#         full_instructions = read_instructions(args.trace_file)
-
-#         print_results()
-#         print_samples(full_instructions, num_print=20, debug=args.debug)    
+                                # reset for next set of instructions
+                                next_full_instruct = None
+        return instruction_list
 
 
-# if __name__ == '__main__':
-#         main()
+def main():
+        args = parse_args()
+        print_header(sys.argv, args)
+
+        print_cache_info()
+        full_instructions = read_instructions(args.trace_file)
+
+        print_results()
+        print_samples(full_instructions, num_print=20, debug=args.debug)    
+
+
+if __name__ == '__main__':
+        main()
