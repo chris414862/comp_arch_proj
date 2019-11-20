cb66ed9067ef1c08838d989cc850a29692a43256
chris414862 <chris414862@gmail.com>
2019-11-20 18:54:49 +0000

    Just about done

--- Sim.py
+++ Sim.py
@@ -9,110 +9,97 @@ from ctypes import c_uint
 import random
 from collections import Counter
 
-# -f <trace file name>     [ name of text file with the trace ]
-# -s <cache block size>    [ 1 KB to 8 MB ]
-# -b <block size>          [ 4 bytes to 64 bytes ]
-# -a <associativity>       [ 1, 2, 4, 8, 16 ]
-# -r <replacement policy>  [RR or RND or LRU for bonus points]
 
 DEFAULT_EXECUTION_CYCLES = 2
 DEFAULT_MEM_ACCESS_CYCLES = 3
 BUS_SIZE = 4
 GLOBAL_CLOCK = 0
+DEBUG = False
+DEBUG_ITERS = 10
+ALIGN = 30
+H_ALIGN = 20
+H_SIZE = 50
+DEBUG_ALIGN = 15
+REP_STRINGS = {'RND':'Random', 'RR':'Round Robin', 'LRU':'Least Recently Used'}
 
-def main():
-	
+def main():	
 	args = parse_args()
 	print_header(sys.argv, args)
 	cache = Cache(cache_size=args.cache_size, block_size=args.block_size, associativity=args.associativity
 				 , rep_policy=args.rep_policy)
 	cache.display()
 	full_instructions = read_instructions(args.trace_file)
-	hit_rate, miss_rate, cpi, tot_hits, tot_misses,tot_accesses = cache_simulator(cache, full_instructions)
-	print_results(hit_rate, miss_rate, cpi, tot_hits, tot_misses, tot_accesses)
-	if args.debug:
-		print_samples(full_instructions, num_print=20)   
+	results = cache_simulator(cache, full_instructions)
+	print_results(*results)
+	if DEBUG:
+		print_samples(full_instructions, num_print=DEBUG_ITERS)   
 
 
 def cache_simulator(cache, full_instructions):
 	num_instructions = len(full_instructions)
-	tot_mem_accesses = 0
-	tot_hits = 0
-	tot_misses = 0
-	tot_cycles = 0
-	counts = Counter()
-	for g, full_instruction in enumerate(full_instructions):
+	tot_mem_accesses = tot_hits = tot_misses = tot_comp_misses = tot_cycles = 0
+	for i, full_instruction in enumerate(full_instructions):
+
 		num_remaining_requests = full_instruction.get_num_mem_requests()
-		
-		counts[num_remaining_requests] +=1
-		#print(num_remaining_requests)
 		while num_remaining_requests > 0:
 			next_request, num_remaining_requests = full_instruction.next_mem_request()
-			# print('curr request:', next_request.to_string())
-			# print('num_remaining:', num_remaining_requests)
-			hits, misses, cycles = process_request(next_request, cache,g)
-			hits = hits #if hits <= 1 else 1
-			misses = misses #if misses <= 1 else 1
-			tot_hits += hits #if hits <= 1 else 1
-			tot_misses += misses #if misses <= 1 else 1
+			hits, misses, cycles, comp_misses = process_request(next_request, cache)
+			tot_hits += hits 
+			tot_misses += misses 
 			tot_cycles += cycles
 			tot_mem_accesses += hits +misses
-			#counts[cycles] +=1 
-			#print(hits, misses, cycles)
-		# if g >= 1000:
-		# 	num_instructions = g
-		# 	break
-		# 	print(tot_hits, tot_misses, tot_cycles)
-		# 	sys.exit()
-	print(counts)
-	# s = sum([int(val)*int(key) for key,val in counts.items()])
-	# [print(str(key)+': '+str(int(val)*int(key)/s)) for key,val in counts.items()]
-	# print(num_instructions)
-	denom = tot_hits + tot_misses
-	return tot_hits/denom, tot_misses/denom, tot_cycles/num_instructions, tot_hits, tot_misses, tot_mem_accesses
+			tot_comp_misses += comp_misses
 
-def process_request(mem_access_request, cache, n):
+		# Stop short for debugging
+		if DEBUG:
+			if i >= DEBUG_ITERS:
+				num_instructions = i 
+				break
+
+	return tot_cycles, num_instructions, tot_hits, tot_misses, tot_mem_accesses, tot_comp_misses
+
+def process_request(mem_access_request, cache):
 	global GLOBAL_CLOCK
-	#print('global clock:', GLOBAL_CLOCK)
+
 	cycles = DEFAULT_EXECUTION_CYCLES
 	bytes_to_read = mem_access_request.length
 	address = mem_access_request.address
-	hits = 0
-	misses = 0
-	#t = 0
-	# print('\nxxxxxxx start address:%x'%address)
-	# print('xxxxxxx start address:', bin(address))
+	logging.debug(f"\n       {'start address:':{DEBUG_ALIGN}}{address:x}")
+	logging.debug(f"{'start address:':{DEBUG_ALIGN}}{address:b}")
+
+	hits = misses = comp_misses = 0
 	while bytes_to_read > 0:
-		# print('xxxxxxx new address:%x'%address)
-		# print('xxxxxxx new address:', bin(address))
-		# print('bytes to read:', bytes_to_read)
 		GLOBAL_CLOCK = GLOBAL_CLOCK + 1
-		# print('global clock:', GLOBAL_CLOCK)
+		logging.debug(f"{'new address:':{DEBUG_ALIGN}}{address:x}")
+		logging.debug(f"{'new address:':{DEBUG_ALIGN}}{address:b}")
+		logging.debug(f"{'bytes to read:':{DEBUG_ALIGN}}{bytes_to_read}")
+		logging.debug(f"{'global clock:':{DEBUG_ALIGN}}{GLOBAL_CLOCK}")
+
 		bytes_read= cache.read_from_cache(address, bytes_to_read)
 		
-		
 		if bytes_read <= 0: # miss
-			# print('MISS!', n)
+			logging.debug(f"{'MISS!':{DEBUG_ALIGN}} ")
+
+			comp_misses = comp_misses +1 if bytes_read == -1 else comp_misses 
 			cache.replace_block(address)
 			bytes_read= cache.read_from_cache(address, bytes_to_read)
 			misses += 1
-			
 			cycles += DEFAULT_MEM_ACCESS_CYCLES * math.ceil(cache.block_size/BUS_SIZE)+1
-			
 
 		else: # hit 
-			# print('HIT!')
+			logging.debug(f"{'HIT!':{DEBUG_ALIGN}}")
+
 			cycles += 1
 			hits += 1
-		# print('bytes read from cache:', bytes_read)
-		# print('cycles:', cycles)
+
+		logging.debug(f"{'bytes read from cache:':{DEBUG_ALIGN}}{bytes_read}")
+		logging.debug(f"{'cycles:':{DEBUG_ALIGN}}{cycles}")
+		logging.debug(f"{'bytes remaining to read:':{DEBUG_ALIGN}}{bytes_to_read}")
+
 		bytes_to_read -= bytes_read
 		address += bytes_read
-		# print('bytes remaining to read:', bytes_to_read)
-		# t += 1
-		# if t >5:
-		# 	return hits, misses, cycles
-	return hits, misses, cycles
+		
+	return hits, misses, cycles, comp_misses
 
 	
 
@@ -140,38 +127,36 @@ class CacheRow():
 		self.col_index = col_index
 		self.cols = [Block(col_index=a) for a in range(associativity)]
 		self.next_for_round_robin = 0
+
+
+	def is_full(self):
+		return [c for c in self.cols if not c.valid] == []
 		
 
 	def contains_valid_tag(self, tag, p=False):
-		# if p:
-		# 	print('####CHECKING####', tag)
 		for block in self.cols:
-			# if p:
-			# 	print(block.to_string())
 			if block.tag == tag and block.valid:
 				block.update_clock()
 				return True
+
 		return False
 
 	def replace(self, idx, tag):
 		if type(idx) != int:
 			idx = int(idx)
 		x = self.cols.pop(idx)
-			# print(tag, 'clock for evicted:', x.last_used, ' curr clock:', GLOBAL_CLOCK)
-
 		self.cols.insert(idx, Block(tag=tag, valid=1, col_index=idx))
+
 
 class Cache():
 	def __init__ (self, cache_size=0, block_size=0, associativity=0, rep_policy=None):
 		self.cache_size = cache_size * 1024
-		#print('ssssss ', self.cache_size)
 		self.block_size = block_size
 		self.associativity = associativity
 		self.rep_policy = rep_policy
-		
 		if not math.log2(self.cache_size).is_integer():
 			self.cache_size = 2**math.floor(math.log(self.cache_size,2))
-			logging.warning('Cache size is not a power of 2. Reducing cache size to '+str(self.cache_size//1024)+'kb')
+			logging.warning(f"Cache size is not a power of 2. Reducing cache size to {self.cache_size//1024} kb")
 		self.total_blocks = self.cache_size // self.block_size
 		self.number_of_indeces = math.ceil(self.cache_size  /(self.block_size * self.associativity))  # of indexes with associativity
 		self.block_offset_bits = math.ceil(math.log(block_size, 2)) # get the offset bits
@@ -185,52 +170,37 @@ class Cache():
 	def replace_block(self, address):
 		tag, idx, b_offset = self.get_address_pieces(address)
 		row = self.c[idx]
-		# print('idx:',idx)
-		# row.contains_valid_tag(tag, p=True)
-		# print('next for rr:', row.next_for_round_robin)
 		if self.rep_policy == 'RND':
 			rem_idx = random.randint(0, row.associativity-1)
-			# print('rem_idx:', rem_idx)
-			# row.replace(rem_idx, tag)
+
+			# Make sure invalid blocks are filled first
+			while not row.is_full() and row.cols[rem_idx].valid:
+				rem_idx = random.randint(0, row.associativity-1)
+
 		elif self.rep_policy == 'RR':
 			rem_idx = row.next_for_round_robin
-			# print('rem_idx:', rem_idx)
-			# row.replace(rem_idx, tag)
 			row.next_for_round_robin = (row.next_for_round_robin+1) % row.associativity
+
 		elif self.rep_policy == 'LRU':
 			rem_idx = row.cols.index(min(row.cols, key=lambda x: x.last_used))
-		# print('rem_idx:', rem_idx)
+
 		row.replace(rem_idx, tag)
 
-		#return BUS_SIZE if BUS_SIZE < (self.block_size - b_offset) else (self.block_size - b_offset)
 
 	def read_from_cache(self, address, bytes_to_read):
 		tag, idx, b_offset = self.get_address_pieces(address)
-		# print('!!address:', bin(address))
-		# print(address)
-		# print('!!b_offset:', bin(b_offset))
-		# print(b_offset)
-		# print('!!tag:', bin(tag))
-		# print(tag)
-		# print('!!idx:', bin(idx))
-		# print(idx)
-		# print('!!bob:',self.block_offset_bits)
-		# print('!!idxbits:',self.index_bits)
-		# print(len(self.c))
 		row = self.c[idx]
 		if row.contains_valid_tag(tag):
 			spill_over = (b_offset +BUS_SIZE) > self.block_size and (b_offset +bytes_to_read) > self.block_size 
 			if spill_over:
 				bytes_read = self.block_size - b_offset 
-				#print('!!SPILL OVER!!bytes read:', bytes_read)
+
 			else:
 				bytes_read = bytes_to_read if  bytes_to_read < BUS_SIZE else BUS_SIZE
-				#print('!!!!bytes read:', bytes_read)
 
 			return bytes_read 
 		else:
-
-			return 0
+			return 0 if row.is_full() else -1 # encode whether miss was compulsory
 
 
 	def get_address_pieces(self, address):
@@ -249,17 +219,18 @@ class Cache():
 
 
 	def display(self):
-		#TODO: fill out actual values after implementing calculations
-		print('Cache Size:', self.cache_size)
-		print('Block Size:', self.block_size)
-		print('Associativity:', self.associativity)
-		print('R-Policy:', self.rep_policy)
-		print('----- Calculated Values -----')
-		print('Total #Blocks:', int(self.total_blocks)  )
-		print('Tag Size:', self.tag_bits)
-		print('Index Size:', int(self.index_bits))
-		print('Overhead Memory Size:', self.overhead)
-		print('Implementation Memory Size:', self.implementation)	
+		print(f"\n{'Cache Input Parameters':-^{H_SIZE}}\n")
+		print(f"{'Cache Size:':<{ALIGN}}{self.cache_size/1024:,} kb")
+		print(f"{'Block Size:':<{ALIGN}}{self.block_size:,} bytes")
+		print(f"{'Associativity:':<{ALIGN}}{self.associativity}")
+		print(f"{'Replacement Policy:':<{ALIGN}}{REP_STRINGS[self.rep_policy]}")
+		print(f"\n{'Cache Calculated Parameters':-^{H_SIZE}}\n")
+		print(f"{'Total # Blocks:':<{ALIGN}}{self.total_blocks:,}")
+		print(f"{'Total # Rows:':<{ALIGN}}{self.number_of_indeces:,}")
+		print(f"{'Tag Bits:':<{ALIGN}}{self.tag_bits}")
+		print(f"{'Index Bits:':<{ALIGN}}{self.index_bits}")
+		print(f"{'Overhead Memory Size:':<{ALIGN}}{self.overhead:,} bytes")
+		print(f"{'Implementation Memory Size:':<{ALIGN}}{self.implementation//1024:,} kb")	
 
 
 class MemAccessRequest():
@@ -309,6 +280,7 @@ class full_instruction():
 		self.instruct_line = instruct_line
 		self.rw_line=rw_line
 
+
 	def next_mem_request(self):
 		next_mem_request = self.curr_mem_request
 		num_remaining = self._get_num_remaining()
@@ -338,6 +310,7 @@ class full_instruction():
 		if i is not None:
 			self.curr_mem_request = self.mem_accesses[i]
 
+
 	def set_mem_accesses(self):
 		self.mem_accesses = [self.instruction, self.read, self.write]
 
@@ -349,57 +322,61 @@ class full_instruction():
 				accesses += 1
 		return accesses
 
+
 	def display(self, num_print=3):
 		instruction_count = 1
 
 		logging.debug('***instruction request:')
-		print(self.instruction.to_string())
 		logging.debug('from line: '+str(self.instruct_line))
+
+		print(self.instruction.to_string())
+		
 
 		# Used to make sure only 20 total accesses are printed
 		if instruction_count == num_print:
 			return instruction_count
 
 		logging.debug('write request:')
+		logging.debug('from line: '+str(self.rw_line))
 		if self.write is not None:
 			instruction_count += 1
 			print(self.write.to_string())
 		else:
 			logging.debug(self.write)
-		logging.debug('from line: '+str(self.rw_line))
+
 
 		# Used to make sure only 20 total accesses are printed
 		if instruction_count == num_print:
 			return instruction_count
 
 		logging.debug('read request:')
+		logging.debug('from line: '+str(self.rw_line))
 		if self.read is not None:
 			instruction_count += 1
 			print(self.read.to_string())
 		else:
 			logging.debug(self.read)
-		logging.debug('from line: '+str(self.rw_line))       
+		       
 
 		return instruction_count
 
 
     
 def print_header(cmd_args, parsed_args):
-    print('\nCmd Line:', ' '.join(cmd_args))
-    print('Trace File:', parsed_args.trace_file)
+    print(f"\n{'Cmd Line:':<{H_ALIGN}}{' '.join(cmd_args)}")
+    print(F"{'Trace File:':<{H_ALIGN}}{parsed_args.trace_file}")
 
 
-
-
-def print_results(hit_rate, miss_rate, cpi, tot_hits, tot_misses, tot_accesses):
-    #TODO: fill out results once cache is implemented
-    print('----- Results -----')
-    print('Cache Hit Rate: %.2f%%'%( hit_rate*100))
-    print('Cache Miss Rate: %.2f%%'%(miss_rate*100))
-    print('CPI: %.2f'%(cpi), '\n')
-    print('tot hits:', tot_hits)
-    print('tot misses:', tot_misses)
-    print('tot accesses:', tot_accesses)
+def print_results(tot_cycles, num_instructions, tot_hits, tot_misses, tot_mem_accesses, tot_comp_misses):
+    print(f"\n{'Cache Calculated Parameters':-^{H_SIZE}}\n")
+    print(f"{'Cache Hit Rate:':<{ALIGN}}{tot_hits/tot_mem_accesses:.3%}")
+    print(f"{'Cache Miss Rate:':<{ALIGN}}{tot_misses/tot_mem_accesses:.3%}")
+    print(f"{'CPI:':<{ALIGN}}{tot_cycles/num_instructions:.3f}")
+    print(f"{'Total Cache Accesses:':<{ALIGN}}{tot_mem_accesses:,}")
+    print(f"{'Total Hits:':<{ALIGN}}{tot_hits:,}")
+    print(f"{'Total Misses:':<{ALIGN}}{tot_misses:,}")
+    print(f"----{'Total Compulsory Misses:':<{ALIGN-4}}{tot_comp_misses:,}")
+    print(f"----{'Total Conflict Misses:':<{ALIGN-4}}{tot_misses -tot_comp_misses:,}")
 
 
 def print_samples(instructions, num_print=20):
@@ -409,6 +386,7 @@ def print_samples(instructions, num_print=20):
             if count >= num_print:
                     logging.debug('num_print reached: '+str(num_print))
                     break
+
 
 def read_instructions(trace_file):
     if not path.exists(trace_file):
@@ -449,18 +427,17 @@ def read_instructions(trace_file):
                             next_full_instruct.read = read
                             next_full_instruct.rw_line = line
                             next_full_instruct.set_mem_accesses()
-                            if next_full_instruct.instruction.length > 0:
-                                    instruction_list.append(next_full_instruct)
+                            if next_full_instruct.instruction.length >= 0:
+                                instruction_list.append(next_full_instruct)
 
                             # reset for next set of instructions
                             next_full_instruct = None
     return instruction_list
 
 
-
-
-
 def parse_args():
+	global DEBUG
+	global DEBUG_ITERS
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-f', dest='trace_file', metavar='filename', type=str
 					   , required=True, help='filename of the trace file')
@@ -478,7 +455,8 @@ def parse_args():
 					   , choices=['RR','RND','LRU'], required=True
 					   , help='Replacement policy. Acceptable values: RR (round'\
 					   +' robin), RND (random), LRU (least recently used)')
-	parser.add_argument('-d', dest='debug', action='store_true', help='Add extra print statements')
+	parser.add_argument('-d', dest='debug', default=-1, nargs='?', const=DEBUG_ITERS
+					   ,help='Add extra print statements')
 	args = parser.parse_args()
 	if not (1 <= args.cache_size <= 8192):
 		parser.error('-s cache size is out of range.  int between 1 and 8192') 
@@ -488,23 +466,25 @@ def parse_args():
 	if args.block_size not in [2**i for i in range(2, 7)]:
 		parser.error('-b block size is not a power of 2. Select int between 4 and 64') 
 
+	if args.debug >= 0:
+			DEBUG = True
+			DEBUG_ITERS = args.debug
+
 	fmt = "%(levelname)s: %(message)s"	
 	logging.basicConfig(filename=sys.argv[0]+'.log', format=fmt, level=logging.INFO)
 	
 	# adds logging to console (for WARNING and CRITICAL levels only)
 	consoleHandler = logging.StreamHandler(sys.stdout)
 	consoleHandler.setFormatter(logging.Formatter(fmt))
-	if args.debug:
+	if DEBUG:
 		consoleHandler.setLevel(logging.DEBUG)
 	else:
-		consoleHandler.setLevel(logging.WARNING)
+		consoleHandler.setLevel(logging.INFO)
 	logging.getLogger().addHandler(consoleHandler)
 
 	# shows debug logging
 	if args.debug:
 		logging.getLogger().setLevel(logging.DEBUG)
-
-		
 
 	return args
 
